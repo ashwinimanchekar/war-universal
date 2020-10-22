d36485fc066f7a84b40db9eef30253f7a161e45e
neosoft-jaykantj <neosoft_jaykantj@vfsglobal.com>
2020-10-22 19:11:49 +0530

    Link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   baseURL: 'https://war-api.herokuapp.com',
   hostedURL: 'https://war-universal.herokuapp.com/',
   facebookShare: 'https://www.facebook.com/sharer/sharer.php?u=',
-  whatsappShare: 'https://api.whatsapp.com/send?&amp;text='
+  whatsappShare: 'https://api.whatsapp.com/send?&text='
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   baseURL: 'https://war-api.herokuapp.com',
   hostedURL: 'https://war-universal.herokuapp.com/',
   facebookShare: 'https://www.facebook.com/sharer/sharer.php?u=',
-  whatsappShare: 'https://api.whatsapp.com/send?&amp;text='
+  whatsappShare: 'https://api.whatsapp.com/send?&text='
 };
 
 /*
