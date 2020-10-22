a84b9eddbf29db56ffbf07929b5cedf52555f408
neosoft-jaykantj <neosoft_jaykantj@vfsglobal.com>
2020-10-22 18:51:40 +0530

    Hosted url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   baseURL: 'https://war-api.herokuapp.com',
-  hostedURL: 'https://what-a-relief.herokuapp.com/',
+  hostedURL: 'https://war-universal.herokuapp.com/',
   facebookShare: 'https://www.facebook.com/sharer/sharer.php?u=',
   whatsappShare: 'https://api.whatsapp.com/send?&amp;text='
 };
