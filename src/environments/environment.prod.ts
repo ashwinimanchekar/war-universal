5f071dbf75c4ba1cb68143eecb06c13b005c9c4e
neosoft-jaykantj <neosoft_jaykantj@vfsglobal.com>
2020-10-23 12:22:44 +0530

    Env updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
-  baseURL: 'https://war-api.herokuapp.com',
-  hostedURL: 'https://war-universal.herokuapp.com/',
+  baseURL: 'https://whatarelief.in/api',
+  // baseURL: 'https://war-api.herokuapp.com',
+  // hostedURL: 'https://war-universal.herokuapp.com/',
+  hostedURL: 'https://whatarelief.in/',
   facebookShare: 'https://www.facebook.com/sharer/sharer.php?u=',
   whatsappShare: 'https://api.whatsapp.com/send?&text='
 };
